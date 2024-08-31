80e820ebee886b2a53929ce4342ca375e13a32a4
eNKay <nguyenkhai24082408@gmail.com>
2024-08-31 22:40:11 +0700

    feat: Add constants for navigation, contents, assets
    Added constants for navigation links and imported assets and sample contents.

---
+++ src/constants/index.js
@@ -0,0 +1,212 @@
+import {
+	people01,
+	people02,
+	people03,
+	facebook,
+	instagram,
+	linkedin,
+	twitter,
+	airbnb,
+	binance,
+	coinbase,
+	dropbox,
+	send,
+	shield,
+	star,
+} from "../assets";
+
+export const navLinks = [
+	{
+		id: "home",
+		title: "Trang Chủ",
+	},
+	{
+		id: "features",
+		title: "Tính Năng",
+	},
+	{
+		id: "product",
+		title: "Sản Phẩm",
+	},
+	{
+		id: "clients",
+		title: "Khách Hàng",
+	},
+];
+
+export const features = [
+	{
+		id: "feature-1",
+		icon: star,
+		title: "Phần Thưởng",
+		content:
+			"Những thẻ tín dụng tốt nhất mang đến sự kết hợp hấp dẫn của các chương trình khuyến mãi và giải thưởng.",
+	},
+	{
+		id: "feature-2",
+		icon: shield,
+		title: "100% Bảo Mật",
+		content:
+			"Chúng tôi thực hiện các bước chủ động để đảm bảo thông tin và giao dịch của bạn an toàn.",
+	},
+	{
+		id: "feature-3",
+		icon: send,
+		title: "Chuyển Đổi Số Dư",
+		content:
+			"Thẻ tín dụng chuyển đổi số dư có thể giúp bạn tiết kiệm rất nhiều tiền từ các khoản phí lãi suất.",
+	},
+];
+
+export const feedback = [
+	{
+		id: "feedback-1",
+		content:
+			"Tiền chỉ là một công cụ. Nó sẽ đưa bạn đến bất cứ đâu bạn muốn, nhưng nó sẽ không thay thế bạn làm người điều khiển.",
+		name: "Mỹ Khánh",
+		title: "Nhà Sáng Lập & Lãnh Đạo",
+		img: people01,
+	},
+	{
+		id: "feedback-2",
+		content:
+			"Tiền làm cuộc sống của bạn dễ dàng hơn. Nếu bạn may mắn có nó, bạn thật sự may mắn.",
+		name: "Đức Khải",
+		title: "Nhà Sáng Lập & Lãnh Đạo",
+		img: people02,
+	},
+	{
+		id: "feedback-3",
+		content:
+			"Thường thì những người làm việc trong lĩnh vực tiền tệ và thương mại quốc tế mới là những người thực sự giàu có.",
+		name: "Văn Chiến",
+		title: "Nhà Sáng Lập & Lãnh Đạo",
+		img: people03,
+	},
+];
+
+export const stats = [
+	{
+		id: "stats-1",
+		title: "Người Dùng Hoạt Động",
+		value: "3800+",
+	},
+	{
+		id: "stats-2",
+		title: "Công Ty Tin Cậy",
+		value: "230+",
+	},
+	{
+		id: "stats-3",
+		title: "Giao Dịch",
+		value: "$230M+",
+	},
+];
+
+export const footerLinks = [
+	{
+		title: "Liên Kết Hữu Ích",
+		links: [
+			{
+				name: "Nội Dung",
+				link: "https://www.enkaybank.com/content/",
+			},
+			{
+				name: "Cách Hoạt Động",
+				link: "https://www.enkaybank.com/how-it-works/",
+			},
+			{
+				name: "Tạo Mới",
+				link: "https://www.enkaybank.com/create/",
+			},
+			{
+				name: "Khám Phá",
+				link: "https://www.enkaybank.com/explore/",
+			},
+			{
+				name: "Điều Khoản & Dịch Vụ",
+				link: "https://www.enkaybank.com/terms-and-services/",
+			},
+		],
+	},
+	{
+		title: "Cộng Đồng",
+		links: [
+			{
+				name: "Trung Tâm Hỗ Trợ",
+				link: "https://www.enkaybank.com/help-center/",
+			},
+			{
+				name: "Đối Tác",
+				link: "https://www.enkaybank.com/partners/",
+			},
+			{
+				name: "Gợi Ý",
+				link: "https://www.enkaybank.com/suggestions/",
+			},
+			{
+				name: "Blog",
+				link: "https://www.enkaybank.com/blog/",
+			},
+			{
+				name: "Bản Tin",
+				link: "https://www.enkaybank.com/newsletters/",
+			},
+		],
+	},
+	{
+		title: "Đối Tác",
+		links: [
+			{
+				name: "Đối Tác Chúng Tôi",
+				link: "https://www.enkaybank.com/our-partner/",
+			},
+			{
+				name: "Trở Thành Đối Tác",
+				link: "https://www.enkaybank.com/become-a-partner/",
+			},
+		],
+	},
+];
+
+export const socialMedia = [
+	{
+		id: "social-media-1",
+		icon: instagram,
+		link: "https://www.instagram.com/",
+	},
+	{
+		id: "social-media-2",
+		icon: facebook,
+		link: "https://www.facebook.com/",
+	},
+	{
+		id: "social-media-3",
+		icon: twitter,
+		link: "https://www.twitter.com/",
+	},
+	{
+		id: "social-media-4",
+		icon: linkedin,
+		link: "https://www.linkedin.com/",
+	},
+];
+
+export const clients = [
+	{
+		id: "client-1",
+		logo: airbnb,
+	},
+	{
+		id: "client-2",
+		logo: binance,
+	},
+	{
+		id: "client-3",
+		logo: coinbase,
+	},
+	{
+		id: "client-4",
+		logo: dropbox,
+	},
+];
